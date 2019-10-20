f35758aadd75e2de13a4b82281a913705c9bde27
Zojae031 <edgar3@naver.com>
2019-10-20 18:40:48 +0900

    Edit TecDialog Data Setting

--- app/src/main/java/zojae031/portfolio/tec/TecDialog.kt
+++ app/src/main/java/zojae031/portfolio/tec/TecDialog.kt
@@ -2,7 +2,6 @@ package zojae031.portfolio.tec
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import zojae031.portfolio.R
 import zojae031.portfolio.base.BaseFragmentDialog
@@ -18,38 +17,6 @@ class TecDialog :
         binding.vm = tecViewModel
         binding.dialog = this
     }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-//        JsonParser().parse(data.source).asJsonArray.map { element ->
-//            with(element.asJsonObject) {
-//                setButton(left, get("data1").asString, get("left").asString)
-//                setButton(right, get("data2").asString, get("right").asString)
-//            }
-//        }
-
-    }
-
-
-    private fun setButton(button: Button, url: String, buttonName: String) {
-        if (url == "" || buttonName == "") {
-            button.visibility = View.GONE
-        } else {
-            button.apply {
-                text = buttonName
-                setOnClickListener {
-                    context.startActivity(
-                        TecActivity.getIntent(
-                            context,
-                            url
-                        )
-                    )
-                }
-            }
-        }
-    }
-
 
 }
 
--- app/src/main/java/zojae031/portfolio/util/bindingAdapterUtil.kt
+++ app/src/main/java/zojae031/portfolio/util/bindingAdapterUtil.kt
@@ -1,12 +1,16 @@
 package zojae031.portfolio.util
 
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.google.gson.JsonParser
 import zojae031.portfolio.R
 import zojae031.portfolio.base.BaseRecyclerViewAdapter
+import zojae031.portfolio.tec.TecActivity
 
 @BindingAdapter(value = ["loadUrlCircle"])
 fun ImageView.showImageCircle(url: String?) {
@@ -36,5 +40,47 @@ fun ImageView.showImage(url: String?) {
 fun RecyclerView.ReplaceAll(list: List<Any>?) {
     if (list != null) {
         (this.adapter as? BaseRecyclerViewAdapter<Any, *>)?.updateLists(list)
+    }
+}
+
+@BindingAdapter(value = ["leftButtonSet"])
+fun Button.left(arr: String) {
+    JsonParser().parse(arr).asJsonArray.map { element ->
+        with(element.asJsonObject) {
+            this@left.text = get("left").asString.also {
+                if (it == "") {
+                    visibility = View.GONE
+                }
+                this@left.setOnClickListener {
+                    context.startActivity(
+                        TecActivity.getIntent(
+                            context,
+                            get("data1").asString
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
+
+@BindingAdapter(value = ["rightButtonSet"])
+fun Button.right(arr: String) {
+    JsonParser().parse(arr).asJsonArray.map { element ->
+        with(element.asJsonObject) {
+            this@right.text = get("right").asString.also {
+                if (it == "") {
+                    visibility = View.GONE
+                }
+                this@right.setOnClickListener {
+                    context.startActivity(
+                        TecActivity.getIntent(
+                            context,
+                            get("data2").asString
+                        )
+                    )
+                }
+            }
+        }
     }
 }
