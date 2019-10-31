82cdefb5ed453fd2a0ef3002fc541dc10d0b0725
Spock <spockwoz@gmail.com>
2019-10-31 22:53:01 +0900

    for code review

--- app/src/main/java/zojae031/portfolio/data/dao/main/MainEntity.kt
+++
@@ -1,7 +0,0 @@
-package zojae031.portfolio.data.dao.main
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
-data class MainEntity(@PrimaryKey val userImage: String, val notice: String)
