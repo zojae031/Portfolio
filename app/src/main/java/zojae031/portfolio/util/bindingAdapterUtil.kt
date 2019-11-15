aa5a1056bff45c68a77130a8f80e5238959f19f5
Zojae031 <edgar3@naver.com>
2019-11-15 21:28:30 +0900

    Edit Tec Button Util

--- app/src/main/java/zojae031/portfolio/util/bindingAdapterUtil.kt
+++ app/src/main/java/zojae031/portfolio/util/bindingAdapterUtil.kt
@@ -1,18 +1,16 @@
 package zojae031.portfolio.util
 
-import android.view.View
-import android.widget.Button
 import android.widget.ImageView
+import androidx.annotation.MainThread
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.google.gson.JsonParser
 import zojae031.portfolio.R
 import zojae031.portfolio.base.SimpleRecyclerViewAdapter
-import zojae031.portfolio.tec.TecActivity
 
 @BindingAdapter(value = ["loadUrlCircle"])
+@MainThread
 fun ImageView.showImageCircle(url: String?) {
     Glide.with(this)
         .load(url)
@@ -24,6 +22,7 @@ fun ImageView.showImageCircle(url: String?) {
 }
 
 @BindingAdapter(value = ["loadUrl"])
+@MainThread
 fun ImageView.showImage(url: String?) {
     Glide
         .with(this)
@@ -35,50 +34,9 @@ fun ImageView.showImage(url: String?) {
 }
 
 @BindingAdapter(value = ["replaceAll"])
+@MainThread
 fun RecyclerView.replaceAll(list: List<Any>?) {
     if (list != null) {
         (this.adapter as? SimpleRecyclerViewAdapter<Any, *>)?.updateLists(list)
-    }
-}
-
-@BindingAdapter(value = ["leftButtonSet"])
-fun Button.left(arr: String) {
-    JsonParser().parse(arr).asJsonArray.map { element ->
-        with(element.asJsonObject) {
-            this@left.text = get("left").asString.also {
-                if (it == "") {
-                    visibility = View.GONE
-                }
-                this@left.setOnClickListener {
-                    context.startActivity(
-                        TecActivity.getIntent(
-                            context,
-                            get("data1").asString
-                        )
-                    )
-                }
-            }
-        }
-    }
-}
-
-@BindingAdapter(value = ["rightButtonSet"])
-fun Button.right(arr: String) {
-    JsonParser().parse(arr).asJsonArray.map { element ->
-        with(element.asJsonObject) {
-            this@right.text = get("right").asString.also {
-                if (it == "") {
-                    visibility = View.GONE
-                }
-                this@right.setOnClickListener {
-                    context.startActivity(
-                        TecActivity.getIntent(
-                            context,
-                            get("data2").asString
-                        )
-                    )
-                }
-            }
-        }
     }
 }
